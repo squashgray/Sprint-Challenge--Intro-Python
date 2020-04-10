7259a28cec8a2a76417703b9bcc737256036b45c
squashgray <squashgray@gmail.com>
2020-04-10 08:28:00 -0700

    oop2.py

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,11 +4,19 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
 
-    # TODO
+    def drive(self):
+        return "vroooom"
 
+  
+class Motorcycle(GroundVehicle):
+    def __init__(self,num_wheels=2):
+        super().__init__(num_wheels=2)
+
+    def drive(self):
+         return "BRAAAP!!"
 
 # Subclass Motorcycle from GroundVehicle.
 #
